02363a1e27234420e04e56f78c7094523f9d0b93
Nguyễn Hoài Thương <40071306+gruviamon@users.noreply.github.com>
2020-04-28 11:25:42 +0700

    updatemain.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     char* suits[SUITS] = {"Hearts", "Diamonds", "Clubs", "Spades"};
     char* faces[FACES] = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
-    int deck[SUITS][FACES] = {0};
+    int deck[SUITS][FACES];
     srand(time(NULL));
 
     int choice;
@@ -16,7 +16,7 @@ int main()
         Menu(choice, suits, faces, deck);
         system("pause");
         system("cls");
-    } while (choice >= 0 && choice < 5);
+    } while (choice >= 0 && choice <= 5);
     return 0;
 }
 
